02bf326f02f9e7593e6f2c0f376b69d59a7e19c8
DanielMChaves <daniel.melero.95@gmail.com>
2017-03-11 00:40:19 +0100

    Project Structure

---
+++ src/initial/Launcher.java
@@ -0,0 +1,12 @@
+package initial;
+
+/**
+ * Created by dmelero on 11/03/2017.
+ */
+public class Launcher {
+
+    public static void main (String[] args){
+        System.out.println("ADW");
+    }
+
+}

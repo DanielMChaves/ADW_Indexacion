28fde928994b0a97b40ae4a65c41fe96d6f368a8
DanielMChaves <daniel.melero.95@gmail.com>
2017-03-27 17:03:39 +0200

    Project Structure

--- src/initial/Init.java
+++ src/initial/Init.java
@@ -3,7 +3,7 @@ package initial;
 /**
  * Created by dmelero on 11/03/2017.
  */
-public class Launcher {
+public class Init {
 
     public static void main (String[] args){
         System.out.println("ADW");
